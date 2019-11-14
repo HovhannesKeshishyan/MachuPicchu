2a6bf4bd62251bb69c7769a86c4c048bfc828d15
Hovhannes1991 <hovhanneskeshishyan@yandex.ru>
2019-11-14 16:39:32 +0400

    Updates

---
+++ precache-manifest.3775e2a747ba3c6970e896a14461a70c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf3e2a88cdf7bdd8d10d7c75f71802d",
+    "url": "/MachuPicchu/index.html"
+  },
+  {
+    "revision": "375c4e8a5eee74785e04",
+    "url": "/MachuPicchu/static/css/main.545f77c8.chunk.css"
+  },
+  {
+    "revision": "5db9287fac7919143a93",
+    "url": "/MachuPicchu/static/js/2.05a4e089.chunk.js"
+  },
+  {
+    "revision": "375c4e8a5eee74785e04",
+    "url": "/MachuPicchu/static/js/main.96df206c.chunk.js"
+  },
+  {
+    "revision": "23cd71ee8f1347d64670",
+    "url": "/MachuPicchu/static/js/runtime-main.b03553ca.js"
+  }
+]);
